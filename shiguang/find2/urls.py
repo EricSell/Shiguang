a1f01eed0a1d4523a17d7ee0ae816d70d321b11c
Eric_sell <YuyanG1997>
2019-08-16 21:40:12 +0800

    7.find2 百科列表ok 百科内容未完成 v2.1.1

--- shiguang/find2/urls.py
+++ shiguang/find2/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from find2 import views
 app_name = 'find2'
 urlpatterns = [
-
-    path("index/", views.index)
+    path("baikelist/", views.baike_list, name='baike_list')
 ]
--- shiguang/find2/views.py
+++ shiguang/find2/views.py
@@ -1,6 +1,43 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from find1.models import Baike
 
-# Create your views here.
-def index(request):
-    return HttpResponse("OK!")
+
+def baike_list(request):
+    if request.method == "GET":
+        baikes = Baike.objects.all()
+        baike_name = []
+        for baike in baikes:
+            baike_name.append(baike.name)
+        data = {
+            'status': 1,
+            'msg': 'ok',
+            'data':
+                {
+                    'baike_name': baike_name
+                }
+        }
+        return JsonResponse(data)
+    else:
+        return JsonResponse({
+            'status': 0,
+            'msg': 'fail',
+        })
+
+
+def baike_detail(request):
+    if request.method == "GET":
+        baikes = Baike.objects.all()
+        baike_name = []
+        baike_content = []
+        for baike in baikes:
+            baike_name.append(baike.name)
+
+        data = {
+            'status': 1,
+            'msg': 'ok',
+            'data': {
+                'baike_name': baike_name,
+                'baike_content': baike_content,
+            }
+        }
