09c68420cbce8e5fad9a7074901147a70e4c26ee
Eric_sell <YuyanG1997>
2019-08-18 13:40:15 +0800

    find2 finish

--- shiguang/find2/views.py
+++ shiguang/find2/views.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-from find1.models import Baike, BaikeShowLike
-
+from find1.models import Baike, BaikeShowLike, Menu, MenuShouLike
+from index.models import User, Follow
 
 # 百科详情
 def baike_detail(request):
     if request.method == "GET":
         baikeid = request.GET.get('baikeid')
         # 百科的关注数量与收藏数量
-        love_sum=0
-        collect_sum=0
+        love_sum = 0
+        collect_sum = 0
         baike_love_coll_sum = BaikeShowLike.objects.filter(baike_id=baikeid)
         if baike_love_coll_sum.exists():
             love_sum = baike_love_coll_sum.filter(love=True).count()
@@ -18,17 +17,20 @@ def baike_detail(request):
         # 用户是否关注
         # user = request.user
         # userid = user.id
-        userid = 1
+        userid = 4
         if userid:
-            user_love = int(BaikeShowLike.objects.filter(user_id=userid).first().love)
-            user_collect = int(BaikeShowLike.objects.filter(user_id=userid).first().collect)
+            user = BaikeShowLike.objects.filter(user_id=userid)
+            if user:
+                user_love = int(user.first().love)
+                user_collect = int(user.first().collect)
+            else:
+                user_love = 0
+                user_collect = 0
 
         # 获取百科详情
         baikes = Baike.objects.filter(id=baikeid)
         if baikes.exists():
-            print(baikes)
             baike = baikes.first()
-            print(baike)
             baike_name = baike.name
             baike_detail = baike.content
             baike_img = baike.img
@@ -38,11 +40,11 @@ def baike_detail(request):
                 'baike_love': love_sum,
                 'baike_collect': collect_sum,
                 'baike_img': baike_img,
-                'user_love':user_love,
-                'user_collect':user_collect,
+                'user_love': user_love,
+                'user_collect': user_collect,
             }
             data = {
-                'code': 0,
+                'code': 1,
                 'msg': 'ok',
                 'data': {
                     'baike_content': baike_content,
@@ -52,8 +54,8 @@ def baike_detail(request):
 
         else:
             return JsonResponse({
-                'code': 1002,
-                'msg': '没有这篇文章',
+                'code': 1001,
+                'msg': '参数错误',
             })
     return JsonResponse({
         'code': 1003,
@@ -66,16 +68,16 @@ def baike_detail_love(request):
     if request.method == "GET":
 
         # user = request.user
-        user=1
+        user = 1
         baikeid = request.GET.get('baikeid')
-        print(baikeid)
         if user:
             if Baike.objects.filter(id=baikeid).first():
                 # userid = user.id
                 userid = 1
-                is_love = BaikeShowLike.objects.filter(user_id=userid, baike_id=baikeid,love=True).first()
-                love = BaikeShowLike.objects.filter(user_id=userid, baike_id=baikeid).first()
-                if love:
+                is_love = BaikeShowLike.objects.filter(user_id=userid, baike_id=baikeid, love=True).first()
+                love = BaikeShowLike.objects.filter(user_id=userid, baike_id=baikeid)
+                if love.exists():
+                    love = love.first()
                     if is_love:
                         love.love = 0
                         love.save()
@@ -83,14 +85,13 @@ def baike_detail_love(request):
                         love.love = 1
                         love.save()
                     return JsonResponse({
-                        'code': 0,
+                        'code': 1,
                         'msg': 'ok',
                         'data': {
                             'is_love': love.love,
                         }
                     })
                 else:
-                    print(userid,baikeid)
                     baikeshoulike = BaikeShowLike()
                     baikeshoulike.love = 1
                     baikeshoulike.collect = 0
@@ -98,18 +99,19 @@ def baike_detail_love(request):
                     baikeshoulike.baike_id = baikeid
                     baikeshoulike.save()
                     return JsonResponse({
-                        'code':1001,
-                        'msg':'添加数据成功',
+                        'code': 1,
+                        'msg': '添加数据成功',
+                        'data': baikeshoulike.love,
                     })
             else:
                 return JsonResponse({
-                    'code': 1002,
-                    'msg': '没有这篇文章',
+                    'code': 1001,
+                    'msg': '参数错误',
                 })
         else:
             return JsonResponse({
-               'code': 1004,
-               'msg': '用户未登录',
+                'code': 1002,
+                'msg': '未登录',
             })
 
     else:
@@ -126,14 +128,14 @@ def baike_detail_collect(request):
         # user = request.user
         user = 1
         baikeid = request.GET.get('baikeid')
-        print(baikeid)
         if user:
             if Baike.objects.filter(id=baikeid).first():
                 # userid = user.id
                 userid = 1
                 is_collect = BaikeShowLike.objects.filter(user_id=userid, baike_id=baikeid, collect=True).first()
-                collect = BaikeShowLike.objects.filter(user_id=userid, baike_id=baikeid).first()
-                if collect:
+                collect = BaikeShowLike.objects.filter(user_id=userid, baike_id=baikeid)
+                if collect.exists():
+                    collect = collect.first()
                     if is_collect:
                         collect.collect = 0
                         collect.save()
@@ -141,14 +143,13 @@ def baike_detail_collect(request):
                         collect.collect = 1
                         collect.save()
                     return JsonResponse({
-                        'code': 0,
+                        'code': 1,
                         'msg': 'ok',
                         'data': {
                             'is_collect': collect.collect,
                         }
                     })
                 else:
-                    print(userid, baikeid)
                     baikeshoulike = BaikeShowLike()
                     baikeshoulike.love = 0
                     baikeshoulike.collect = 1
@@ -156,20 +157,20 @@ def baike_detail_collect(request):
                     baikeshoulike.baike_id = baikeid
                     baikeshoulike.save()
                     return JsonResponse({
-                        'code': 1001,
+                        'code': 1,
                         'msg': '添加数据成功',
+                        'data': baikeshoulike.collect,
                     })
             else:
                 return JsonResponse({
-                    'code': 1002,
-                    'msg': '没有这篇文章',
+                    'code': 1001,
+                    'msg': '参数错误',
                 })
         else:
             return JsonResponse({
-                'code': 1004,
-                'msg': '用户未登录',
+                'code': 1002,
+                'msg': '未登录',
             })
-
     else:
         return JsonResponse({
             'code': 1003,
@@ -179,4 +180,161 @@ def baike_detail_collect(request):
 
 # 用户列表
 def user_list(request):
-    pass
+    if request.method == "GET":
+        users = User.objects.all()
+        user_list = []
+        if users.exists():
+            for user in users:
+                user_id = user.id
+                user_name = user.username
+                user_icon = user.icon
+                user_content = {
+                    'user_id': user_id,
+                    'user_name': user_name,
+                    'user_icon': user_icon,
+                }
+                user_list.append(user_content)
+            data = {
+                'code': 1,
+                'msg': 'ok',
+                'data': {
+                    'user_content': user_list,
+                }
+            }
+            return JsonResponse(data)
+    else:
+        return JsonResponse({
+            'code': 1003,
+            'msg': '请求方式有误',
+        })
+
+
+# 用户详情
+def user_detail(request):
+    if request.method == "GET":
+        otherid = request.GET.get('otherid')
+        try:
+            otherid = int(otherid)
+        except:
+            return JsonResponse({
+                'code': 1001,
+                'msg': '参数错误',
+            })
+        other = User.objects.filter(id=otherid)
+        if other:
+            # 用户的关注数量与粉丝数量
+            other_name = other.first().username
+            other_img = other.first().icon
+            other_intro = other.first().intro
+            fans_sum = Follow.objects.filter(yid_id=otherid).count()
+            collect_sum = Follow.objects.filter(myid_id=otherid).count()
+
+            other_detail = {
+                'other_name': other_name,
+                'other_img': other_img,
+                'other_intro': other_intro,
+                'fans_sum': fans_sum,
+                'collect_sum': collect_sum,
+
+            }
+
+            # 菜谱书与喜欢
+            menu_collect = []
+            menu_id = MenuShouLike.objects.filter(user_id=otherid, collect=True).values('menu_id')
+            menu_id_list = list(menu_id)
+            for menu_id in menu_id_list:
+                menu_id = int(menu_id.get('menu_id'))
+                menu_name = Menu.objects.filter(id=menu_id).first().name
+                menu_img = Menu.objects.filter(id=menu_id).first().img
+                menu_content = {
+                    'menu_id': menu_id,
+                    'menu_name': menu_name,
+                    'menu_img': menu_img,
+                }
+                menu_collect.append(menu_content)
+            menu_love = []
+            menu_id = MenuShouLike.objects.filter(user_id=otherid, love=True).values('menu_id')
+            menu_id_list = list(menu_id)
+            for menu_id in menu_id_list:
+                menu_id = int(menu_id.get('menu_id'))
+                menu_name = Menu.objects.filter(id=menu_id).first().name
+                menu_img = Menu.objects.filter(id=menu_id).first().img
+                menu_content = {
+                    'menu_id': menu_id,
+                    'menu_name': menu_name,
+                    'menu_img': menu_img,
+                }
+                menu_love.append(menu_content)
+
+            return JsonResponse({
+                'code': 1,
+                'msg': 'ok',
+                'data': {
+                    'other_detail': other_detail,
+                    'menu_collect': menu_collect,
+                    'menu_love': menu_love,
+                }
+            })
+        else:
+            return JsonResponse({
+                'code': 1001,
+                'msg': '参数错误',
+            })
+    return JsonResponse({
+        'code': 1003,
+        'msg': '请求方式有误',
+    })
+
+
+# 用户详情-关注
+def user_detail_follow(request):
+    if request.method == "GET":
+
+        # user = request.user
+        user = 1
+        if user:
+            otherid = request.GET.get('otherid')
+            try:
+                otherid = int(otherid)
+            except:
+                return JsonResponse({
+                    'code': 1001,
+                    'msg': '参数错误',
+                })
+            if User.objects.filter(id=otherid).first():
+                # userid = user.id
+                userid = 1
+                flag = 0
+                follow = Follow.objects.filter(myid_id=userid, yid_id=otherid).first()
+                if follow:
+                    follow.delete()
+                    flag = 0
+                else:
+                    follow = Follow()
+                    follow.myid_id = userid
+                    follow.yid_id = otherid
+                    follow.save()
+                    flag = 1
+                return JsonResponse({
+                    'code': 1,
+                    'msg': 'success',
+                    'data': {
+                        'is_follow': flag,
+                    }
+                })
+            else:
+                return JsonResponse({
+                    'code': 1001,
+                    'msg': '参数错误',
+                })
+        else:
+            return JsonResponse({
+                'code': 1002,
+                'msg': '未登录',
+            })
+
+    else:
+        return JsonResponse({
+            'code': 1003,
+            'msg': '请求方式有误',
+        })
